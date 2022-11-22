4aa0e78682f420a2d6c06e6b03a7eb1565e93855
kdb11 <kevin.badwi@medieinstitutet.se>
2022-11-22 17:42:12 +0100

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ var objectUsers = [
 	}
 ]
 
-function login() {
+/* function login() {
 	
 	var username = document.getElementById('username').value
 	
@@ -22,8 +22,23 @@ function login() {
 			console.log("incorrect username or password")
 		}
 	}
-}
+} */
 
 const loginBtn = document.getElementById("loginBtn");
 loginBtn.addEventListener("click", function login() {
+	
+	var username = document.getElementById('username').value
+	
+	var password = document.getElementById('password').value
+
+    for(var i = 0; i < objectUsers.length; i++) {
+		
+		if(username == objectUsers[i].username && password == objectUsers[i].password) {
+			console.log(username + " is logged in!")
+            document.body.style.backgroundImage = "url('Assets/windowsBackground.jpg')";
+			break
+		} else {
+			console.log("incorrect username or password")
+		}
+	}
 });
